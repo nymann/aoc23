fa37483606a0df71d542a7514a1c51eebdc6dd32
Kristian Jakobsen <kristian@nymann.dev>
2023-12-02 09:42:20 +0100

    Rename test example data

--- tests/unit_tests/test_d01.py
+++ tests/unit_tests/test_d01.py
@@ -2,10 +2,10 @@ from aoc23.day_01.solver import Day1Solver
 
 
 def test_p1() -> None:
-    solver = Day1Solver(input_file="tests/unit_tests/d1.txt")
+    solver = Day1Solver(input_file="tests/data/d01_p1.example")
     assert solver.p1() == 142
 
 
 def test_p2() -> None:
-    solver = Day1Solver(input_file="tests/unit_tests/d1_p2.txt")
+    solver = Day1Solver(input_file="tests/data/d01_p2.example")
     assert solver.p2() == 281
