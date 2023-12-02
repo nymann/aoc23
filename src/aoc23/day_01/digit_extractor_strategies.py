b8f327703fe60cc55d0f184ff47f87e273d884c1
Kristian Jakobsen <kristian@nymann.dev>
2023-12-02 03:12:53 +0100

    Refactor

--- src/aoc23/day_01/digit_extractor_strategies.py
+++ src/aoc23/day_01/digit_extractor_strategies.py
@@ -14,13 +14,13 @@ class DigitExtractorStrategy(ABC):
 
 class DigitExtractor(DigitExtractorStrategy):
     def first(self, line: str) -> int:
-        for character in line:
-            if character.isdigit():
-                return int(character)
-        raise RuntimeError(line)
+        return self._first_digit(line)
 
     def last(self, line: str) -> int:
-        for character in line[::-1]:
+        return self._first_digit(line[::-1])
+
+    def _first_digit(self, line: str) -> int:
+        for character in line:
             if character.isdigit():
                 return int(character)
         raise RuntimeError(line)
@@ -39,34 +39,20 @@ class WordDigitExtractor(DigitExtractorStrategy):
             "eight": 8,
             "nine": 9,
         }
-        self.reversed_digit_words = {
-            "eno": 1,
-            "owt": 2,
-            "eerht": 3,
-            "ruof": 4,
-            "evif": 5,
-            "xis": 6,
-            "neves": 7,
-            "thgie": 8,
-            "enin": 9,
-        }
+        self.reversed_digit_words = {word[::-1]: digit for word, digit in self.digit_words.items()}  # noqa: WPS221
 
     def first(self, line: str) -> int:
+        return self._find_digit(line, self.digit_words)
+
+    def last(self, line: str) -> int:
+        return self._find_digit(line[::-1], self.reversed_digit_words)
+
+    def _find_digit(self, line: str, digit_words: dict[str, int]) -> int:
         for index, character in enumerate(line):
             if character.isdigit():
                 return int(character)
             line_sub = line[index:]
-            for word, digit in self.digit_words.items():
+            for word, digit in digit_words.items():
                 if line_sub.startswith(word):
-                    return digit
-        raise RuntimeError(line)
-
-    def last(self, line: str) -> int:
-        reversed_line = line[::-1]
-        for index, character in enumerate(reversed_line):
-            if character.isdigit():
-                return int(character)
-            for word, digit in self.reversed_digit_words.items():
-                if reversed_line[index:].startswith(word):
                     return digit
         raise RuntimeError(line)
